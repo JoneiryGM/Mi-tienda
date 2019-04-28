471b0331c68a4f6f502ecaf91a043fa7b4a1cce2
Joneiry <joneiri98@gmail.com>
2019-04-28 13:35:01 -0400

    requeri los layout en el index

--- index_maquetacion.php
+++ index_maquetacion.php
@@ -9,9 +9,18 @@
 <body>
     <!-- LAS SECCIONES DEL INDEX: -->
     <!-- 1-CEBECERA -->
+    <?php require_once 'views/layout_page_principal/cabecera.php';?>
+
     <!-- 2-MENU -->
+    <?php require_once 'views/layout_page_principal/menu.php';?>
+
     <!-- 3-BARRA LATERAL -->
+    <?php require_once 'views/layout_page_principal/barra_lateral.php';?>
+
     <!-- 4-CONTENIDO CENTRAL -->
+    <?php require_once 'views/layout_page_principal/contenido_central.php';?>
+
     <!-- 5-PIE DE PAGINA -->
+    <?php require_once 'views/layout_page_principal/pie_pagina.php';?>
 </body>
 </html>
