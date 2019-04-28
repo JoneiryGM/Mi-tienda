a647c7f44c9f519669f19db75c55e088a87e5552
Joneiry <joneiri98@gmail.com>
2019-04-28 13:00:01 -0400

    Comprobando el los metodos recibidos por get

---
+++ index.php
@@ -0,0 +1,27 @@
+<h1>Bienvenido a mi WEB</h1>
+<?php
+
+require_once 'autoload.php';
+
+
+if(isset($_GET['controller']))
+{
+    $nombre_controlador = $_GET['controller'].'Controller';
+    echo $nombre_controlador;
+}else{
+    echo 'la pagina no existe';
+    exit();
+}
+
+
+if (isset($nombre_controlador) && class_exists($nombre_controlador)) {
+    
+    $controlador = new $nombre_controlador();
+   
+     if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
+         $action = $_GET['action'];
+         $controlador->$action();
+     }else{
+        echo "La metodo que buscas no existe";
+     }
+}
