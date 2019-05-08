830d0fa11163c6c15ab6e553acbbc1df15dbc785
Joneiry <joneiri98@gmail.com>
2019-05-08 12:46:42 -0400

    maquetado del index, casi completo

--- index_maquetacion.php
+++ index_maquetacion.php
@@ -69,27 +69,27 @@
         </aside>
 
         <!-- 4-CONTENIDO CENTRAL -->
-        <div class="central"> 
-
+        <div id="central"> 
+           <h1>Productos destacados</h1>
            <div class="product">
              <img src="assets/img/ceta.png" alt="">
              <h2>Camiseta Negra sport</h2>
              <p>30 dolares</p>
-             <a href="">Comprar</a>
+             <a href="" class="buttom">Comprar</a>
            </div>
 
            <div class="product">
              <img src="assets/img/ceta2.jpg" alt="">
              <h2>Camiseta Negra sport</h2>
              <p>30 dolares</p>
-             <a href="">Comprar</a>
+             <a href="" class="buttom">Comprar</a>
            </div>
 
            <div class="product">
              <img src="assets/img/ceta3.jpg" alt="">
              <h2>Camiseta Negra sport</h2>
              <p>30 dolares</p>
-             <a href="">Comprar</a>
+             <a href="" class="buttom">Comprar</a>
            </div>
         </div>
     </div>
