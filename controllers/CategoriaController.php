18cd5a040b94c96381b556193a71fb8106011356
Joneiry <joneiri98@gmail.com>
2019-05-17 15:27:01 -0400

    crear categoria listo

--- controllers/CategoriaController.php
+++ controllers/CategoriaController.php
@@ -19,5 +19,15 @@ class CategoriaController
     {
         utils::isAdmin();
         
+        // comprobar los datos por POST
+        if(isset($_POST) && isset($_POST['nombre']))
+        {
+            // guardar la categoria a la BD
+            $categoria = new CategoriaModel();
+            $categoria->setNombre($_POST['nombre']);
+            $categoria->save();
+        }
+
+        header("Location:".base_url."categoria/Index");
     }
 }
--- models/CategoriaModel.php
+++ models/CategoriaModel.php
@@ -26,12 +26,27 @@ class CategoriaModel
         $this->id = $id;
     }
     public function setNombre($nombre){
-        $this->nombre = $nombre;
+        $this->nombre = $this->db->real_escape_string($nombre);
     }
 
     public function getcategorias()
     {
-        $query = $this->db->query("SELECT * FROM categoria");
+        $query = $this->db->query("SELECT * FROM categoria ORDER BY id DESC");
         return $query;
+    }
+
+    public function save()
+    {
+        $sql = "INSERT INTO categoria VALUES(NULL,'{$this->getNombre()}')";
+        $registrar = $this->db->query($sql);  
+        $resultado=false;
+
+        if($registrar){
+          $resultado=true;
+        }else{
+            $resultado=false;
+        }
+
+        return $resultado;
     }
 }
